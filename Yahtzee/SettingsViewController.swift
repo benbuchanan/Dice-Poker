3df7b6084bfb22555d45d4a3074c2e6449ab5718
Benjamin BuchBuchanan <blbuchan@ncsu.edu>
2020-05-04 14:55:15 -0400

    added settings button and view controller

---
+++ Yahtzee/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Yahtzee
+//
+//  Created by Ben Buchanan on 5/4/20.
+//  Copyright © 2020 Ben Buchanan. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
